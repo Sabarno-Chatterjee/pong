66e7889d9a7c3c3b12a631d8a5379dbeb97def37
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-18 17:35:33 +0530

    paddle collision functional

--- ball.py
+++ ball.py
@@ -6,6 +6,7 @@ class Ball(Turtle):
         super().__init__()
         self.shape("circle")
         self.color("white")
+        self.speed(0)
         self.penup()
         self.shapesize(stretch_len=1, stretch_wid=1)
         self.x_move = 10
@@ -16,6 +17,8 @@ class Ball(Turtle):
         y_position = self.ycor() + self.y_move
         self.goto(x=x_position, y=y_position)
 
-    def bounce(self):
-        self.x_move *= -1
+    def bounce_y(self):
         self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
--- main.py
+++ main.py
@@ -10,25 +10,32 @@ screen.title("Pong")
 screen.listen()
 screen.tracer(0)
 
-paddle1 = Paddle((350, 0))
-paddle2 = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 is_game_on = True
 
 ball = Ball()
 
-screen.onkey(fun=paddle1.move_up, key="Up")
-screen.onkey(fun=paddle1.move_down, key="Down")
-screen.onkey(fun=paddle2.move_up, key="w")
-screen.onkey(fun=paddle2.move_down, key="s")
+screen.onkey(fun=r_paddle.move_up, key="Up")
+screen.onkey(fun=r_paddle.move_down, key="Down")
+screen.onkey(fun=l_paddle.move_up, key="w")
+screen.onkey(fun=l_paddle.move_down, key="s")
 
 while is_game_on:
-    ball.ball_movement()
+    time.sleep(0.09)
     screen.update()
-    time.sleep(0.1)
+    ball.ball_movement()
+
     # Detects collision with top or bottom wall:
-    if ball.ycor() > 300 or ball.ycor() < -300:
-        print("Numbnuts")
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        print("bounce")
         # need to bounce
-        ball.bounce()
+        ball.bounce_y()
+
+    # Detect collision with paddle:
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        print("made contact")
+        ball.bounce_x()
 
 screen.exitonclick()
