8be5e333f67e9e23c7e3c7f79968378c833415ae
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-27 21:39:43 +0530

    using pygame module to add new look, generates a 1000X600 screen

--- main.py
+++ main.py
@@ -1,55 +1,75 @@
-from turtle import Turtle, Screen
-from paddle import Paddle
-from ball import Ball
-from scoreboard import Scoreboard
-import time
+# from turtle import Turtle, Screen
+# from paddle import Paddle
+# from ball import Ball
+# from scoreboard import Scoreboard
+# import time
+#
+# screen = Screen()
+# screen.setup(width=800, height=600)
+# screen.bgcolor("black")
+# screen.title("Pong")
+# screen.listen()
+# screen.tracer(0)
+#
+# r_paddle = Paddle((350, 0))
+# l_paddle = Paddle((-350, 0))
+# is_game_on = True
+#
+# ball = Ball()
+#
+# screen.onkey(fun=r_paddle.move_up, key="Up")
+# screen.onkey(fun=r_paddle.move_down, key="Down")
+# screen.onkey(fun=l_paddle.move_up, key="w")
+# screen.onkey(fun=l_paddle.move_down, key="s")
+#
+# scoreboard = Scoreboard()
+#
+# while is_game_on:
+#     time.sleep(ball.ball_speed)
+#     screen.update()
+#     ball.ball_movement()
+#
+#     # Detects collision with top or bottom wall:
+#     if ball.ycor() > 280 or ball.ycor() < -280:
+#         # need to bounce
+#         ball.bounce_y()
+#
+#     # Detect collision with paddle:
+#
+#     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+#         ball.bounce_x()
+#         ball.ball_speed *= 0.9
+#
+#     # When right paddle misses the ball:
+#     if ball.xcor() > 390:
+#         ball.reset_position()
+#         ball.ball_speed = 0.1
+#         scoreboard.l_point()
+#
+#     # When left paddle misses the ball:
+#     if ball.xcor() < -390:
+#         ball.reset_position()
+#         ball.ball_speed = 0.1
+#         scoreboard.r_point()
+#
+# screen.exitonclick()
 
-screen = Screen()
-screen.setup(width=800, height=600)
-screen.bgcolor("black")
-screen.title("Pong")
-screen.listen()
-screen.tracer(0)
+import pygame
 
-r_paddle = Paddle((350, 0))
-l_paddle = Paddle((-350, 0))
-is_game_on = True
 
-ball = Ball()
+# Game constants
+WIDTH = 1000
+HEIGHT = 600
 
-screen.onkey(fun=r_paddle.move_up, key="Up")
-screen.onkey(fun=r_paddle.move_down, key="Down")
-screen.onkey(fun=l_paddle.move_up, key="w")
-screen.onkey(fun=l_paddle.move_down, key="s")
+# Design of screen:
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-scoreboard = Scoreboard()
 
-while is_game_on:
-    time.sleep(ball.ball_speed)
-    screen.update()
-    ball.ball_movement()
+running = True
+while running:
 
-    # Detects collision with top or bottom wall:
-    if ball.ycor() > 280 or ball.ycor() < -280:
-        # need to bounce
-        ball.bounce_y()
+    for event in pygame.event.get():
+        if event == pygame.QUIT:
+            running = False
 
-    # Detect collision with paddle:
 
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
-        ball.bounce_x()
-        ball.ball_speed *= 0.9
-
-    # When right paddle misses the ball:
-    if ball.xcor() > 390:
-        ball.reset_position()
-        ball.ball_speed = 0.1
-        scoreboard.l_point()
-
-    # When left paddle misses the ball:
-    if ball.xcor() < -390:
-        ball.reset_position()
-        ball.ball_speed = 0.1
-        scoreboard.r_point()
-
-screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,20 +1,22 @@
-from turtle import Turtle
+# from turtle import Turtle
+#
+#
+# class Paddle(Turtle):
+#     def __init__(self, position):
+#         super().__init__()
+#         self.shape("square")
+#         self.color("white")
+#         self.penup()
+#         self.speed("fastest")
+#         self.shapesize(stretch_wid=5, stretch_len=1)
+#         self.goto(position)
+#
+#     def move_up(self):
+#         y_position = self.ycor() + 20
+#         self.goto(x=self.xcor(), y=y_position)
+#
+#     def move_down(self):
+#         y_position = self.ycor() - 20
+#         self.goto(x=self.xcor(), y=y_position)
+import pygame
 
-
-class Paddle(Turtle):
-    def __init__(self, position):
-        super().__init__()
-        self.shape("square")
-        self.color("white")
-        self.penup()
-        self.speed("fastest")
-        self.shapesize(stretch_wid=5, stretch_len=1)
-        self.goto(position)
-
-    def move_up(self):
-        y_position = self.ycor() + 20
-        self.goto(x=self.xcor(), y=y_position)
-
-    def move_down(self):
-        y_position = self.ycor() - 20
-        self.goto(x=self.xcor(), y=y_position)
