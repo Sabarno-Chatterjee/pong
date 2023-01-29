488f77e4d1e6e2f5383efd81b7b41ded7ecfeb76
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-29 16:29:07 +0530

    tried to add automation, works alright, except centre scenario

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ paddle1_y_change = 0
 paddle2_y_change = 0
 game_ball_X = 492
 game_ball_Y = 292
-game_ball_X_change = -4
+game_ball_X_change = -8
 game_ball_Y_change = 4
 paddle1_score = 0
 paddle2_score = 0
@@ -144,6 +144,9 @@ while running:
             if event.key == pygame.K_s:
                 paddle2_y_change = 0
 
+
+
+
     # Paddle movements:
     paddle1_y += paddle1_y_change
     paddle2_y += paddle2_y_change
@@ -170,10 +173,12 @@ while running:
     if collision1 < 100 and game_ball_X > 925:
         sound()
         game_ball_X_change = game_ball_X_change * -1
+        paddle2_y = 250
 
     collision2 = distance(game_ball_X, game_ball_Y, paddle2_x, paddle2_y)
     if collision2 < 100 and game_ball_X < 45:
         sound()
+        factor = -1
         game_ball_X_change = game_ball_X_change * -1
 
     if paddle1_y >= 480:
@@ -185,6 +190,43 @@ while running:
         paddle2_y = 480
     elif paddle2_y <= 0:
         paddle2_y = 0
+# Automation code:
+    if game_ball_X < 500:
+        if game_ball_Y > 300:
+            if collision2 < 100:
+                paddle2_y_change = 5
+            elif collision2 < 200:
+                paddle2_y_change = 5
+            elif collision2 < 300:
+                paddle2_y_change = 8
+            elif collision2 < 400:
+                paddle2_y_change = 10
+            elif collision2 < 500:
+                paddle2_y_change = 10
+            elif collision2 < 600:
+                paddle2_y_change = 13
+            elif collision2 < 700:
+                paddle2_y_change = 15
 
+    if game_ball_X < 500:
+        if game_ball_Y < 300:
+            if collision2 < 100:
+                paddle2_y_change = -5
+            elif collision2 < 200:
+                paddle2_y_change = -5
+            elif collision2 < 300:
+                paddle2_y_change = -8
+            elif collision2 < 400:
+                paddle2_y_change = -10
+            elif collision2 < 500:
+                paddle2_y_change = -10
+            elif collision2 < 600:
+                paddle2_y_change = -13
+            elif collision2 < 700:
+                paddle2_y_change = -15
+    if collision2 < 100 and game_ball_X < 45:
+        sound()
+        paddle2_y_change = 0
+        paddle2_y = 250
     pygame.display.flip()
 pygame.quit()
